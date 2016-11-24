89e2aa3bb49528ca973bc853716e551f84394339
Szilard Ledan <szledan@gmail.com>
2016-11-24 11:39:28 +0100

    Revert documentation
    Signed-off-by: Szilard Ledan <szledan@gmail.com>

--- src/arg-parse.hpp
+++ src/arg-parse.hpp
@@ -48,121 +48,28 @@ public:
     typedef std::initializer_list<std::string> OptionList;
     struct Errors;
 
-    /*!
-     * \brief  Main constructor.
-     * \param interlacedOptions  options split by commas witout spaces
-     *
-     * * Examples
-     * \code{.cpp}
-     * ArgParse::ArgParse ap("program.name=my program name,tab=  ,help.show=2,help.add=false");
-     * ap.options.program.name  == "my program name"; // Use this program name in generated help.
-     * ap.options.tab           == "  "; // The 'tab' is two spaces.
-     * ap.options.help.show     == 2; // The '2' means to ShowAll.
-     * ap.options.help.add      == false; // Doesn't add to help flag with default.
-     * \endcode
-     *
-     * \see ArgParse::Options
-     */
     ArgParse(const std::string& interlacedOptions = "");
-    /*!
-     * \brief  Constructor with initializer list.
-     * \param options  a list of option strings
-     *
-     * * Examples
-     * \code{.cpp}
-     * ArgParse::ArgParse ap( { "program.name=my program name",
-     *                          "tab=\t",
-     *                          "help.show=2",
-     *                          "help.add=0" } );
-     * ap.options.program.name  == "my program name"; // Use this program name in generated help.
-     * ap.options.tab           == "\t"; // The 'tab' is a char of TAB.
-     * ap.options.help.show     == 2; // The '2' means to ShowAll.
-     * ap.options.help.add      == false; // Doesn't add to help flag with default.
-     * \endcode
-     *
-     * \see ArgParse::Options
-     */
     ArgParse(const OptionList& options);
 
-    /*!
-     * \brief  Add definitaion of a Flag (aka 'option').
-     * \param flag  defined valid Flag
-     * \param func  a call back function, which call the ArgParse::parse() if recieve in 'argv'
-     * \return  reference of the added Flag, otherwise returns the reference of the Flag::WrongFlag.
-     *
-     * \see Flag::isValid()
-     * \see Flag (Falg::WrongFlag)
-     */
     const Flag& def(const Flag& flag, const CallBackFunc func = nullptr);
-    /*!
-     * \brief  Add definitaion of an Arg (aka 'argument').
-     * \param arg  defined valid Arg
-     * \return  reference of defined Arg, otherwise returns the reference of Arg::WrongArg.
-     *
-     * \see Arg (Arg::WrongArg)
-     */
     const Arg& def(const Arg& arg);
 
-    /*!
-     * \brief  Parse recieved arguments.
-     * \param argc  size of \c argv[]
-     * \param argv  array of arguments
-     * \return  true if has not errors.
-     *
-     * \see ArgParse::Errors
-     */
     const bool parse(const int argc, char* const argv[]);
 
     const std::string help();
     const std::string error();
     const std::vector<Errors>& errors() const;
 
-    /*!
-     * \brief  Check settings of a Flag.
-     * \param flagStr  long or short flag string
-     * \return  \c true if Flag::isSet is \c true, otherwise returns \c false.
-     */
     const bool check(const std::string& flagStr);
-    /*!
-     * \brief  Check setting of a Flag and read Value.
-     * \param flagStr  long or short flag string
-     * \param value  pointer of result value
-     * \return  \c true if Flag::isSet is \c true and reading is succesful, otherwise returns \c false.
-     */
     template<typename T>
     const bool checkAndRead(const std::string& flagStr, T* value);
 
-    /*!
-     * \brief  Operator to get a Flag.
-     * \param idx  long or short flag string
-     * \return  const reference of a Flag or Flag::WrongFlag.
-     */
     Flag const& operator[](const std::string& idx);
-    /*!
-     * \brief  Operator to get a Flag.
-     * \param idx  long or short flag string
-     * \return  const reference of Flag or Flag::WrongFlag.
-     */
     Flag const& operator[](const char* idx);
 
-    /*!
-     * \brief  Operator to get an Arg.
-     * \param idx  unsigned int index of argument
-     * \return  const reference of Arg or Arg::WrongArg
-     */
     Arg const& operator[](const std::size_t& idx);
-    /*!
-     * \brief  Operator to get an Arg.
-     * \param idx  unsigned int index of argument
-     * \return  const reference of Arg or Arg::WrongArg
-     */
     Arg const& operator[](const int idx);
 
-    /*!
-     * \brief  The Counts struct.
-     *
-     * Collection of numbers of defined/undefined flags/args during of parsing \c argv[].
-     */
     struct Counts {
         struct {
             size_t defined;
@@ -170,9 +77,6 @@ public:
         } flags, args;
     } counts = { { 0u, 0u, }, { 0u, 0u } };
 
-    /*!
-     * \brief  The Options struct.
-     */
     struct Options {
         struct { std::string name; } program = { "" };
         std::string tab = "    ";
@@ -185,11 +89,6 @@ public:
         } help = { true, true, Help::ShowAllDefined };
     } options;
 
-    /*!
-     * \brief  The Errors struct.
-     *
-     * Collection of errors during of parsing \c argv[].
-     */
     struct Errors {
         enum Codes {
             NoError = 0,
@@ -272,19 +171,6 @@ struct Flag {
          const std::string& description,
          const Value value);
 
-    /*!
-     * \brief Validate the flag.
-     *
-     * Rules:
-     *  * Invalid a Flag if both \c _longFlag and \c _shortFlag strings are empty.
-     *  * Valid a Flag if \c _longFlag size bigger than \c 2 and first two
-     *    characters equal to the \c '-' char.
-     *  * Valid a Flag if \c _shortFlag size equals to the \c 2 and first character
-     *    is the \c '-'.
-     *  * Otherwise the Flag is invalide.
-     *
-     * \return  result of validation.
-     */
     const bool isValid() const;
 
     bool isSet;
@@ -300,9 +186,6 @@ struct Flag {
 
 // Arg
 
-/*!
- * \brief The Arg is an argument which isn't a Flag (aka option)
- */
 struct Arg : Value {
     static const Arg WrongArg;
 
